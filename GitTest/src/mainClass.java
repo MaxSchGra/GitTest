ee1a98a1630f652cf433f13ae065e06c20ab8e50
Maximilian Schlatter <schlatter.maximilian@hs-furtwangen.de>
2016-11-21 20:42:29 +0100

    zrjztzjt

---
+++ GitTest/src/mainClass.java
@@ -0,0 +1,19 @@
+
+public class mainClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+
+
+		System.out.println("Add a Commit");
+		System.out.println("Add a Commit");
+		
+		
+
+	}
+
+}
